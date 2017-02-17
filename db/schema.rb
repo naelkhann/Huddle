89daaaa4cd7622facd36562c3a282c723f4b63b2
Nael Khan <naelkhann@gmail.com>
2017-02-17 12:10:38 -0500

    Add group seeds and update schema with groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214195159) do
+ActiveRecord::Schema.define(version: 20170217162309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.text     "description", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Group.destroy_all
 
 # Users
 chim4ira312 = User.create(username: "chim4ira312", password: "split312")
 mega0319 = User.create(username: "mega0319", password: "password")
 guest = User.create(username: "Guest", password: "password")
+
+# groups
+vegetarians = Group.create(name: "NYC Vegetarians", description: "Come together and meet with New York City Vegetarians for restaurant trips, cooking sessions, and more!")
+
+overwatchers = Group.create(name: "Overwatchers", description: "All Overwatch players welcome! Learn strategies, make friends, and just have a good time")
+
+watchers = Group.create(name: "Movie Buffs", description: "Meet other movie buffs for viewings, discussions, and more!")
+
+fake_watchers = Group.create(name: "Movie Buffs", description: "We are fake movie buffs!")
