c4602ab693ff8b0cdbe97faba844ad19a0429196
Nael Khan <naelkhann@gmail.com>
2017-02-17 16:17:22 -0500

    Add api/groups routes and tweak users and groups show jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
+    resources :groups, only: [:create, :destroy, :show]
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
