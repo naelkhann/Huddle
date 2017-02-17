ba19160b1b87ab9c23bca27f652d590a67a04501
Nael Khan <naelkhann@gmail.com>
2017-02-17 12:11:20 -0500

    Add annotations and add fix group validation typos

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,17 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Group < ApplicationRecord
   validates :name, :description, presence: true
-  valdiates :name, uniquenes: true
+  validates :name, uniqueness: true
 
   # has_many :user_groups
   # has_many :group_categories
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
