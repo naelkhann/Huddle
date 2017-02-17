cf99d15a88334c913b7f8b31281d979cc4e692a1
Nael Khan <naelkhann@gmail.com>
2017-02-17 16:49:25 -0500

    Add index action for api/groups in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
-    resources :groups, only: [:create, :destroy, :show]
+    resources :groups, only: [:create, :destroy, :show, :index]
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
