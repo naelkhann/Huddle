652cebe749506a55292cbd361ddc17c8ef743204
Nael Khan <naelkhann@gmail.com>
2017-02-17 20:27:33 -0500

    Add categories routes, controller, and jbuilder renders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
+    resources :categories, only: [:index, :show]
     resources :groups, only: [:create, :destroy, :show, :index]
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
