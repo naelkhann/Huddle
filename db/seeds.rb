8111b7eb18b45077cd4f05159e3275027e70a18b
Nael Khan <naelkhann@gmail.com>
2017-02-19 16:32:10 -0500

    Add more categories groups seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,13 @@ overwatchers = Group.create(name: "Overwatchers", description: "All Overwatch pl
 
 watchers = Group.create(name: "Movie Buffs", description: "Meet other movie buffs for viewings, discussions, and more!", moderator_id: chim4ira312.id)
 
-fake_watchers = Group.create(name: "Movie Buffs", description: "We are fake movie buffs!", moderator_id: guest.id)
+smashbros = Group.create(name: "Huddle Up Smash Bros!", description: "All things Super Smash Bros. related! Join tournaments and socialize with other players. Monthly tournaments happening! We embrace all skill levels of players", moderator_id: guest.id)
+
+gourmet = Group.create(name: "The Gourmet Gathering", description: "Are you a foodie? Join our group and meet others who love trying new restaurants and recipes, and love all things food!", moderator_id: chim4ira312.id)
+
+android = Group.create(name: "Androids", description: "A group for everything Android, the mobile operating system by Google", moderator_id: mega0319.id)
+
+rockclimb = Group.create(name: "Rock Climbers Anonymous", description: "Interested in rock climbing? Join this group to see huddles based on rock climbing events, competitions, lessons, and more. Anyone welcome, of all ages and all skill levels", moderator_id: chim4ira312.id)
 
 #Categories
 outdoors_cat = Category.create(name: "Outdoors & Adventure")
@@ -37,3 +43,10 @@ food_cat = Category.create(name: "Food & Drink")
 veg_food = CategoriesGroup.create(group_id: vegetarians.id, category_id: food_cat.id)
 watchers_fam = CategoriesGroup.create(group_id: watchers.id, category_id: fam_cat.id)
 veg_health = CategoriesGroup.create(group_id: vegetarians.id, category_id: health_cat.id)
+android_tech = CategoriesGroup.create(group_id: android.id, category_id: tech_cat.id)
+gourmet_food = CategoriesGroup.create(group_id: gourmet.id, category_id: food_cat.id)
+smashbros_tech = CategoriesGroup.create(group_id: smashbros.id, category_id: tech_cat.id)
+smashbros_fam = CategoriesGroup.create(group_id: smashbros.id, category_id: fam_cat.id)
+rockclimb_fam = CategoriesGroup.create(group_id: rockclimb.id, category_id: fam_cat.id)
+rockclimb_health = CategoriesGroup.create(group_id: rockclimb.id, category_id: health_cat.id)
+rockclimb_outdoors = CategoriesGroup.create(group_id: rockclimb.id, category_id: outdoors_cat.id)
