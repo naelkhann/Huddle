9de012ff25053ee24eccd8648fba54048b012a79
Nael Khan <naelkhann@gmail.com>
2017-02-22 13:39:11 -0500

    Fix logout button style to white font

--- frontend/components/app_buttons.jsx
+++ frontend/components/app_buttons.jsx
@@ -37,7 +37,7 @@ class AppButtons extends React.Component {
       <div className="navbar-buttons-container">
         <h4 className="session-form-label">{this.props.currentUser.username}</h4>
         <Link to="/user"><img className="navbar-profile-img" src={this.props.currentUser.image_url}/></Link>
-        <button className="navbar-button red-button" onClick={this.logoutFn}>Log Out</button>
+        <button id="logout" onClick={this.logoutFn}>Log Out</button>
       </div>
     );
   }
