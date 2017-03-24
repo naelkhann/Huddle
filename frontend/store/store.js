5e7397eaad028be8f526a72ca0d7d4c3c64d44d1
Nael Khan <naelkhann@gmail.com>
2017-03-24 04:33:20 -0400

    Remove logger, add prod webpack to remove minification

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
-const logger = createLogger();
 const configureStore = (preloadedState = {}) => {
   return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   );
 };
 
