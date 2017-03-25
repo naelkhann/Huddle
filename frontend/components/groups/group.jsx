91f0b87222586b67a0f613da56f19a5c6d152ed7
Nael Khan <naelkhann@gmail.com>
2017-03-25 02:46:01 -0400

    Build upcoming and past huddle toggle in Group component

--- frontend/components/groups/group.jsx
+++ frontend/components/groups/group.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getGroup } from '../../actions/groups_actions';
 import { createGroupsUser, deleteGroupsUser } from '../../actions/groups_users_actions';
-import { arrayOfHuddles } from '../../reducers/selectors';
+import { arrayOfHuddles, arrayOfUpcomingHuddles, arrayOfPastHuddles } from '../../reducers/selectors';
 import { Link, withRouter } from 'react-router';
 import LoadingIcon from '../loading_icon';
 
@@ -12,6 +12,8 @@ const mapStateToProps = (state, ownProps) => {
   let isModerator;
   const group = state.group;
   const huddles = arrayOfHuddles(state);
+  const upcoming = arrayOfUpcomingHuddles(state);
+  const past = arrayOfPastHuddles(state);
   const loading = state.loading.loading;
   if(state.session.currentUser){
     userId = state.session.currentUser.id;
@@ -25,6 +27,8 @@ const mapStateToProps = (state, ownProps) => {
   return {
     group,
     huddles,
+    upcoming,
+    past,
     userId,
     isMember,
     isModerator,
@@ -34,7 +38,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   getGroup: (group) => dispatch(getGroup(group)),
-  createGroupsUser: (groups_user) => dispatch(createGroupsUser(groups_user)),
+  createGroupsUser: (groupsUser) => dispatch(createGroupsUser(groupsUser)),
   deleteGroupsUser: (id) => dispatch(deleteGroupsUser(id))
 });
 
@@ -64,7 +68,8 @@ class Group extends React.Component {
         alert("You cannot leave a group you moderate. Go to settings to delete a group");
       }
       else if(this.props.isMember){
-        this.props.deleteGroupsUser(this.props.group.id).then(() => this.props.getGroup(this.props.params.groupId));
+        this.props.deleteGroupsUser(this.props.group.id).then(
+          () => this.props.getGroup(this.props.params.groupId));
       }
       else {
         const membership = {group_id: this.props.group.id, user_id: this.props.userId};
@@ -83,7 +88,8 @@ class Group extends React.Component {
   renderCreatHuddleBtn(){
     if(this.props.isModerator){
       return (
-        <Link to={`groups/${this.props.group.id}/huddles/new`} className="create-huddle-btn">Create A Huddle</Link>
+        <Link to={`groups/${this.props.group.id}/huddles/new`}
+          className="create-huddle-btn">Create A Huddle</Link>
       );
     }
   }
@@ -114,31 +120,67 @@ class Group extends React.Component {
   }
 
   renderHuddles(){
+    const activeLi = this.state.active_li;
     const huddles = this.props.huddles;
-    const huddlesContainers = huddles.map((huddle, idx) => (
-      <div key={idx} className="group-huddle-container">
-        <h2 className="huddle-title">{huddle.title}</h2>
-        <div className="group-huddle-detail-container">
-          <div className="group-huddle-detail-left">
-            <h3 className="huddle-location">{huddle.location}</h3>
-            <h3 className="huddle-description">{huddle.description}</h3>
-          </div>
-          <div className="group-huddle-detail-right">
-            <h3 className="huddle-date">{huddle.day}</h3>
-            <h3 className="huddle-time">{huddle.time}</h3>
-            <h3 className="huddle-time">{`${this.getDaysTilHuddle(huddle.date)} days left`}</h3>
-            <Link to={`/groups/${this.props.group.id}/huddles/${huddle.id}`}>RSVP</Link>
+    const upcomingHuddles = this.props.upcoming;
+    const pastHuddles = this.props.past;
+    let huddlesContainers;
+    if (activeLi === "Upcoming") {
+      huddlesContainers = upcomingHuddles.map((huddle, idx) => (
+        <div key={idx} className="group-huddle-container">
+          <h2 className="huddle-title">{huddle.title}</h2>
+          <div className="group-huddle-detail-container">
+            <div className="group-huddle-detail-left">
+              <h3 className="huddle-location">{huddle.location}</h3>
+              <h3 className="huddle-description">{huddle.description}</h3>
+            </div>
+            <div className="group-huddle-detail-right">
+              <h3 className="huddle-date">{huddle.day}</h3>
+              <h3 className="huddle-time">{huddle.time}</h3>
+              <h3 className="huddle-time">{`${this.getDaysTilHuddle(huddle.date)} days left`}</h3>
+              <Link to={`/groups/${this.props.group.id}/huddles/${huddle.id}`}>RSVP</Link>
+            </div>
           </div>
         </div>
-      </div>
-    ));
-    return huddlesContainers;
+      ));
+
+    } else if(activeLi === "Past") {
+      huddlesContainers = pastHuddles.map((huddle, idx) => (
+        <div key={idx} className="group-huddle-container">
+          <h2 className="huddle-title">{huddle.title}</h2>
+          <div className="group-huddle-detail-container">
+            <div className="group-huddle-detail-left">
+              <h3 className="huddle-location">{huddle.location}</h3>
+              <h3 className="huddle-description">{huddle.description}</h3>
+            </div>
+            <div className="group-huddle-detail-right">
+              <h3 className="huddle-date">{huddle.day}</h3>
+              <h3 className="huddle-time">{huddle.time}</h3>
+              <h3 className="huddle-time">{`${Math.abs(this.getDaysTilHuddle(huddle.date))} days past`}</h3>
+              <Link to={`/groups/${this.props.group.id}/huddles/${huddle.id}`}>See Who Went</Link>
+            </div>
+          </div>
+        </div>
+      ));
+    }
+
+    if(huddlesContainers.length < 1) {
+      return (
+        <div className="group-huddle-none-container">
+          <i className="fa fa-frown-o fa-3x"></i>
+          <h3>There are no planned Huddles at the moment.</h3>
+          <h3>Ask your organizer to make one!</h3>
+        </div>
+      );
+    } else {
+      return huddlesContainers;
+    }
   }
 
   render(){
     const numHuddles = this.props.huddles ? this.props.huddles.length : "";
     const location = this.props.group.location ? this.props.group.location : "";
-    const founded_on = this.props.group.founded_on ? this.props.group.founded_on : "";
+    const foundedOn = this.props.group.founded_on ? this.props.group.founded_on : "";
     const numMembers = this.props.group.members ? this.props.group.members.length : "";
     const organizerImage = this.props.group.moderator ? this.props.group.moderator.image : "";
     const organizerName = this.props.group.moderator ? this.props.group.moderator.name : "";
@@ -159,7 +201,8 @@ class Group extends React.Component {
               <li><a href="#">More</a></li>
             </ul>
             <ul>
-              <button className="group-header-join-btn" onClick={this.joinGroupToggle}>{this.props.isMember ? "Leave Group" : "Join Us!"}</button>
+              <button className="group-header-join-btn"
+                onClick={this.joinGroupToggle}>{this.props.isMember ? "Leave Group" : "Join Us!"}</button>
             </ul>
           </nav>
         </div>
@@ -168,7 +211,7 @@ class Group extends React.Component {
           <div className="group-info">
             <img className="image" src={this.props.group.image} />
             <h3 className="location">{location}</h3>
-            <h3 className="founded_on">Founded On: {founded_on}</h3>
+            <h3 className="founded_on">Founded On: {foundedOn}</h3>
             <p className="num-huddles">Members: {numMembers}</p>
             <p className="num-members">Upcoming Huddles: {numHuddles}</p>
             <h3 className="organizer">Organizer:</h3>
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -4,6 +4,10 @@ export const arrayOfCategories = ({categories}) => values(categories);
 
 export const arrayOfHuddles = ({group}) => values(group.huddles);
 
+export const arrayOfUpcomingHuddles = ({group}) => values(group.upcoming);
+
+export const arrayOfPastHuddles = ({group}) => values(group.past);
+
 export const arrayOfGroups = ({groups}) => values(groups);
 
 export const arrayOfMembers = ({huddle}) => values(huddle.members);
